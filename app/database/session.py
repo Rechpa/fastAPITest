10499e00ed04e623e55bb4f1c9e4c1721b07de06
rechpa <farahdiouani3@gmail.com>
2025-02-21 15:11:43 +0100

    minikube deployment

--- app/database/session.py
+++ app/database/session.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+import os
 # Database URL (will be overridden by environment variables in Docker)
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:password@192.168.1.140:5432/mydatabase"
-
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
